78b50a45338444239d2e30d3767d189a7bfe73c0
Tal <talspektor.gmail.com>
2021-04-05 11:44:39 +0300

    Initial Commit

---
+++ Coupons-SystemTests/Coupons_SystemTests.swift
@@ -0,0 +1,34 @@
+//
+//  Coupons_SystemTests.swift
+//  Coupons-SystemTests
+//
+//  Created by Tal Spektor on 05/04/2021.
+//  Copyright © 2021 Tal Spektor. All rights reserved.
+//
+
+import XCTest
+@testable import Coupons_System
+
+class Coupons_SystemTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
